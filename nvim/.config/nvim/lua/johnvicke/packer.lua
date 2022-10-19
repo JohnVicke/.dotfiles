52981c1f3a20855d8883b060d7688a066c8b8d69
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-19 11:50:33 +0200

    eslint fix all vim

--- nvim/.config/nvim/lua/johnvicke/packer.lua
+++ nvim/.config/nvim/lua/johnvicke/packer.lua
@@ -7,6 +7,7 @@ return require("packer").startup(function()
   use("nvim-treesitter/nvim-treesitter", {
         run = ":TSUpdate"
     })
+
   use("romgrk/nvim-treesitter-context")
 
   use('kyazdani42/nvim-web-devicons')
--- nvim/.config/nvim/lua/johnvicke/remap.lua
+++ nvim/.config/nvim/lua/johnvicke/remap.lua
@@ -1,4 +1,5 @@
 local m = require("johnvicke.keymap")
+local eslint = require("johnvicke.eslint")
 
 m.inoremap("jj", "<ESC>")
 m.nnoremap("<leader>q", ":q<CR>")
@@ -6,3 +7,6 @@ m.nnoremap("<leader>x", ":qa<CR>")
 m.nnoremap("<leader>w", ":w<CR>")
 m.nnoremap("<leader>y", '"+y')
 m.vnoremap("<leader>y", '"+y')
+
+m.nnoremap("<leader>efa", function() eslint.fix_current_file() end)
+
